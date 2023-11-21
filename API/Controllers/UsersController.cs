f5dd251485fbf7f7e0e438015bbbb22dd006a71e
Vitaly <vitaly.korsunov@gmail.com>
2023-11-21 18:25:15 +0200

    End Section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
@@ -20,17 +20,18 @@ namespace API.Controllers
             _context = context;
         }
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>>GetUsers()
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
-             
-        }
 
-         [HttpGet("{Id}")]
-        public async Task<ActionResult<AppUser>>GetUser(int Id)
+        }
+        [Authorize]
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int Id)
         {
-            return await  _context.Users.FindAsync(Id);
-             
+            return await _context.Users.FindAsync(Id);
+
         }
     }
 }
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+       public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+       IConfiguration configuration)
+       {
+            services.AddScoped<ITokenService,TokenService>();
+                services.AddDbContext<DataContext>(option =>
+                {
+                    option.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+                });
+                return services;
+       } 
+    }
+}
