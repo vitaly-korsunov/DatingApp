fecf6719910c17e5aef11ca2c0febabe8499fa24
Vitaly <vitaly.korsunov@gmail.com>
2023-11-28 21:13:48 +0200

    End of saction 8

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,6 +19,8 @@ namespace API.Extensions
        IConfiguration configuration)
        {
             services.AddScoped<ITokenService,TokenService>();
+            services.AddScoped<IUserRepository,UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
                 services.AddDbContext<DataContext>(option =>
                 {
                     option.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
